6880ea50e0527e7a8f10851e8ff44fcb03f6c5b1
negbie <nupagadi@posteo.de>
2020-01-10 20:43:23 +0100

    Add postgres output

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -2,6 +2,7 @@ package all
 
 import (
 	_ "github.com/negbie/telegraf/plugins/outputs/graylog"
+	_ "github.com/negbie/telegraf/plugins/outputs/postgresql"
 	_ "github.com/negbie/telegraf/plugins/outputs/prometheus_client"
 	_ "github.com/negbie/telegraf/plugins/outputs/socket_writer"
 )
